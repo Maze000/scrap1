cc2220fe48af2a1df6fb3503b85e73fc39c0f119
MAZE <72741681+Maze000@users.noreply.github.com>
2024-05-07 01:32:06 +1200

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 module.exports = (app) => {
 
     app.post('/scrape', (req, res) => {
-        const { url, element } = req.body; // Obtiene la URL y el elemento del cuerpo de la solicitud
+        const { url, element } = req.body; 
     
         if (!url || !element) {
             return res.status(400).send('URL and HTML element are required');
@@ -15,28 +15,28 @@ module.exports = (app) => {
             .then(body => {
                 const $ = cheerio.load(body);
                 const result = [];
-                const baseURL = new URL(url).origin; // Obtiene el dominio de la URL de la solicitud
+                const baseURL = new URL(url).origin; 
     
-                // Verifica si el elemento es una imagen
+                
                 if (element.toLowerCase() === 'img') {
                     $(element).each(function() {
                         let src = $(this).attr('src') || '';
     
-                        // Resolver URLs relativas
+                        
                         if (src && !src.startsWith('http')) {
                             src = new URL(src, baseURL).href;
                         }
     
-                        // Filtrar posibles src vacíos o no válidos
+                       
                         if (src.trim() !== '') {
                             result.push(src);
                         }
                     });
                 } else {
-                    // Para otros elementos, simplemente obtiene su texto
+                    
                     $(element).each(function() {
                         const text = $(this).text().trim();
-                        if (text !== '') { // Filtra elementos sin texto
+                        if (text !== '') { 
                             result.push(text);
                         }
                     });
